6d8ed8faa6b2409e1ac770b3a130b033c1633f24
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-07 16:23:31 +0530

    Uper header on hover logout done

--- src/components/commonComponents/upperHeader/UpperHeader.js
+++ src/components/commonComponents/upperHeader/UpperHeader.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './upperHeader.scss';
 import search from '../../../assets/search.png';
-import user from '../../../assets/user.png';
+import user from '../../../assets/dummy-user.jpg';
 import downArrow from '../../../assets/arrow_down.png';
 import logout from '../../../assets/logout.svg';
 
 function UpperHeader() {
-  const [isDropDownOpen, setIsDropDownOPen] = useState(false);
-
+  const history = useHistory();
+  const onLogOut = () => {
+    history.push('/login');
+  };
   return (
     <div className="upper-header-block">
       <div className="upper-header--rounded-block search-block">
@@ -16,21 +19,19 @@ function UpperHeader() {
           <img src={search} />{' '}
         </button>
       </div>
-      <div className="upper-header--rounded-block">
-        <img className="user-dp" src={user} />
-        <label>Michelle Obama</label>
-        <div className="down-arrow" onClick={() => setIsDropDownOPen(!isDropDownOpen)}>
-          <img src={downArrow} />
-          {isDropDownOpen ? (
+      <div className="logout-area">
+        <div className="upper-header--rounded-block">
+          <img className="user-dp" src={user} />
+          <label>Michelle Obama</label>
+          <div className="down-arrow">
+            <img src={downArrow} />
             <div className="user-dropdown">
               <div className="dropdown-option">
                 <img src={logout} />
-                <span>Logout</span>
+                <span onClick={onLogOut}>Logout</span>
               </div>
             </div>
-          ) : (
-            <></>
-          )}
+          </div>
         </div>
       </div>
     </div>
