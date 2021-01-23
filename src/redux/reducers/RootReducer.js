c3d8f9c553cbe2f50a14fa7c8ea869402f3e6690
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-23 12:04:19 +0530

    Added crm in root reducer

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { followUps, opportunityDetail } from './follow-up-reducer/FollowUpReducer';
 import { opportunityNotes } from './follow-up-reducer/notesReducer/NotesReducer';
 import { opportunityHistory } from './follow-up-reducer/historyReducer/HistoryReducer';
+import { crms } from './crm-reducer/CRMReducer';
 
 const rootReducer = combineReducers({
   followUps,
   opportunityDetail,
   opportunityNotes,
   opportunityHistory,
+  crms,
 });
 export default rootReducer;
