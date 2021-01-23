c241c5c3b09e34fe6e9b57be21851e4c2ef49a2d
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-23 16:30:31 +0530

    Added delete opportunity api service

--- src/services/follow-up-service/FollowUpSevice.js
+++ src/services/follow-up-service/FollowUpSevice.js
@@ -7,5 +7,6 @@ const FollowUpService = {
   getOpportunity: id => ApiService.getData(FOLLOW_UP_URL.GET_INDIVIDUAL_OPPORTUNITY_URL + id),
   updateOpportunity: (id, data) =>
     ApiService.putData(FOLLOW_UP_URL.UPDATE_OPPORTUNITY_URL + id, data),
+  deleteOpportunity: id => ApiService.deleteData(FOLLOW_UP_URL.DELETE_OPPORTUNITY_URL + id),
 };
 export default FollowUpService;
