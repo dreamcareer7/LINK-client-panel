f5757e0f89236658b8a97802ff5c225634966629
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-23 14:14:16 +0530

    Added crm graph reducer

--- src/redux/reducers/crm-reducer/CRMReducer.js
+++ src/redux/reducers/crm-reducer/CRMReducer.js
@@ -1,10 +1,46 @@
 import CRM_REDUX_CONSTANT from '../../constants/crmReduxConstants/CRMReduxConstant';
 
-// eslint-disable-next-line import/prefer-default-export
 export const crms = (state = null, action) => {
   switch (action.type) {
     case CRM_REDUX_CONSTANT.GET_FILTERED_CRMS:
       return action.data;
+
+    default:
+      return state;
+  }
+};
+
+const initiralGraphData = {
+  labels: [
+    'Initial Contact',
+    'In Conversation',
+    'Meeting Booked',
+    'Follow-Up',
+    'Potential Deals',
+    'Closed',
+    'Lost',
+  ],
+  datasets: [
+    {
+      data: [0, 0, 0, 0, 0, 0, 0],
+      backgroundColor: '#4282FE',
+    },
+  ],
+};
+
+export const crmsGraphData = (state = initiralGraphData, action) => {
+  switch (action.type) {
+    case CRM_REDUX_CONSTANT.GET_CRMS_GRAPH_DATA:
+      return {
+        ...state,
+        labels: action.data.map(e => e._id),
+        datasets: [
+          {
+            data: action.data.map(e => e.total),
+            backgroundColor: '#4282FE',
+          },
+        ],
+      };
 
     default:
       return state;
