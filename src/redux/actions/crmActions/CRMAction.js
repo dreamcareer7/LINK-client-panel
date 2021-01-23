2d07e580445a323ea13df3625e0c68e0c71083cb
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-23 14:14:07 +0530

    Added crm graph data action

--- src/redux/actions/crmActions/CRMAction.js
+++ src/redux/actions/crmActions/CRMAction.js
@@ -2,7 +2,6 @@ import { errorNotification } from '../../../constants/Toast';
 import CRMService from '../../../services/crm-service/CRMService';
 import CRM_REDUX_CONSTANT from '../../constants/crmReduxConstants/CRMReduxConstant';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getFilteredCRMSAction = (page, data) => {
   return dispatch => {
     CRMService.getFilteredCRMs(page, 9, data)
@@ -13,6 +12,30 @@ export const getFilteredCRMSAction = (page, data) => {
             data: response.data.data,
           });
           if (response.data.data.docs.docs.length === 0) {
+            errorNotification('It seems data not available for this category');
+          }
+        }
+      })
+      .catch(e => {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try after sometime.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        }
+      });
+  };
+};
+
+export const getCRMGraphData = () => {
+  return dispatch => {
+    CRMService.getCRMGraph()
+      .then(response => {
+        if (response.data.status === 'SUCCESS') {
+          dispatch({
+            type: CRM_REDUX_CONSTANT.GET_CRMS_GRAPH_DATA,
+            data: response.data.data,
+          });
+          if (response.data.data.length === 0) {
             errorNotification('It seems data not available for this category');
           }
         }
