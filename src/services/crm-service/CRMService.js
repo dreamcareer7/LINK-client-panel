00edcbe89622eeaaff1cea5a6f31a58fe4815c56
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-23 14:14:38 +0530

    Added crm graph api service

--- src/services/crm-service/CRMService.js
+++ src/services/crm-service/CRMService.js
@@ -4,6 +4,7 @@ import { CRM_URL } from '../../constants/UrlConstant';
 const CRMService = {
   getFilteredCRMs: (page, limit, data) =>
     ApiService.putData(`${CRM_URL.GET_FILTERED_CRM_URL}?page=${page}&limit=${limit}`, data),
+  getCRMGraph: () => ApiService.putData(CRM_URL.GET_CRM_GRAPH_URL),
 };
 
 export default CRMService;
