c2cc24ea3a34e2ff4ab9bac3f9f23d6029e72a04
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-26 16:42:24 +0530

    Added activity break down graph reducer

---
+++ src/redux/reducers/reporting-reducer/ReportingReducer.js
@@ -0,0 +1,32 @@
+import REPORT_REDUX_CONSTANT from '../../constants/reportReduxConstants/ReportReduxConstant';
+
+const initialActivityBreakdownGraphData = {
+  labels: [],
+  datasets: [
+    {
+      data: [],
+      backgroundColor: '#4282FE',
+    },
+  ],
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const activityBreakdownGraphData = (state = initialActivityBreakdownGraphData, action) => {
+  switch (action.type) {
+    case REPORT_REDUX_CONSTANT.GET_ACTIVITY_BREAKDOWN:
+      return {
+        ...state,
+        labels: action.data.map(e => e.label),
+        values: action.data.map(e => e.value),
+        datasets: [
+          {
+            data: action.data.map(e => e.value),
+            backgroundColor: '#4282FE',
+          },
+        ],
+      };
+
+    default:
+      return state;
+  }
+};
