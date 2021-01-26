3971bf2551544aa24ccbfe86059af97d63340396
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-26 18:20:58 +0530

    Added get pipeline values graph data action

--- src/redux/actions/ReportingActions/CRMAction.js
+++ src/redux/actions/ReportingActions/CRMAction.js
@@ -2,7 +2,6 @@ import { errorNotification } from '../../../constants/Toast';
 import ReportingService from '../../../services/reporting-service/ReportingService';
 import REPORT_REDUX_CONSTANT from '../../constants/reportReduxConstants/ReportReduxConstant';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getActivityBreakdownGraphData = ({ startDate, endDate }) => {
   return dispatch => {
     ReportingService.getActivityBreakdown({ startDate, endDate })
@@ -10,6 +9,27 @@ export const getActivityBreakdownGraphData = ({ startDate, endDate }) => {
         if (response.data.status === 'SUCCESS') {
           dispatch({
             type: REPORT_REDUX_CONSTANT.GET_ACTIVITY_BREAKDOWN_GRAPH_DATA,
+            data: response.data.data,
+          });
+        }
+      })
+      .catch(e => {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try after sometime.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        }
+      });
+  };
+};
+
+export const getPipelineValuesGraphData = ({ startDate, endDate }) => {
+  return dispatch => {
+    ReportingService.getPipeline({ startDate, endDate })
+      .then(response => {
+        if (response.data.status === 'SUCCESS') {
+          dispatch({
+            type: REPORT_REDUX_CONSTANT.GET_PIPELINE_VALUES_GRAPH_DATA,
             data: response.data.data,
           });
         }
