7ea78d69847c7dbaf47ef326cafea59f83e97e76
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-26 19:28:36 +0530

    Added total sales graph data action

--- src/redux/actions/ReportingActions/ReportingAction.js
+++ src/redux/actions/ReportingActions/ReportingAction.js
@@ -64,3 +64,24 @@ export const getConversationGraphData = ({ startDate, endDate }) => {
       });
   };
 };
+
+export const getTotalSalesGraphData = ({ startDate, endDate }) => {
+  return dispatch => {
+    ReportingService.getTotalSales({ startDate, endDate })
+      .then(response => {
+        if (response.data.status === 'SUCCESS') {
+          dispatch({
+            type: REPORT_REDUX_CONSTANT.GET_TOTAL_SALES_GRAPH_DATA,
+            data: response.data.data,
+          });
+        }
+      })
+      .catch(e => {
+        if (e.response.data.status === undefined) {
+          errorNotification('It seems like server is down, Please try after sometime.');
+        } else if (e.response.data.status === 'INTERNAL_SERVER_ERROR') {
+          errorNotification('Internal server error');
+        }
+      });
+  };
+};
