d6dbd260a2077d88ecc028773a88b4e3e060e353
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-03 11:44:31 +0530

    Added Backend URLs

--- src/constants/UrlConstant.js
+++ src/constants/UrlConstant.js
@@ -1,4 +1,7 @@
-export const BASE_URL = 'https://link.dev.gradlesol.com/app/';
+export const BASE_URL = 'https://link.dev.gradlesol.com/app/'; // <-- Kevit Dev
+// export const BASE_URL = 'https://link.dev.humanpixel.com.au/app/'; // <-- HP Dev
+// export const BASE_URL = 'https://link.test.humanpixel.com.au/app/'; // <-- HP Staging
+// export const BASE_URL = 'https://jayla.linkfluencer.com/app/'; // <-- Production
 // export const BASE_URL = 'https://0f9d1f408f24.ngrok.io/';
 export const LINKEDIN_CLIENT_ID = '776gktki6ukrgj';
 export const API_METHODS = {
