7aa9397dd85c7dcfac28ec8b0bca44d5ec39e1a3
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-04 10:51:47 +0530

    AccountServices user detail update post call changed to put

--- src/services/account-services/AccountServices.js
+++ src/services/account-services/AccountServices.js
@@ -2,11 +2,11 @@ import ApiService from '../api-service/ApiService';
 import { ACCOUNTS_URL } from '../../constants/UrlConstant';
 
 const AccountService = {
-  updateClient: data => ApiService.postData(ACCOUNTS_URL.UPDATE_CLIENT, data),
+  updateClient: data => ApiService.putData(ACCOUNTS_URL.UPDATE_CLIENT, data),
   getClientInfo: () => ApiService.getData(ACCOUNTS_URL.GET_CLIENT_INFO),
   getIndustry: () => ApiService.getData(ACCOUNTS_URL.GET_INDUSTRY),
   getCompany: () => ApiService.getData(ACCOUNTS_URL.GET_COMPANY_SIZE),
-  creatFCMToken: (data) => ApiService.putData(ACCOUNTS_URL.CREATE_FCM_TOKEN, data),
-  logoutUser: (data) => ApiService.postData(ACCOUNTS_URL.LOGOUT_USER, data)
+  creatFCMToken: data => ApiService.putData(ACCOUNTS_URL.CREATE_FCM_TOKEN, data),
+  logoutUser: data => ApiService.postData(ACCOUNTS_URL.LOGOUT_USER, data),
 };
 export default AccountService;
