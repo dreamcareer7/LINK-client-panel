83a97e944b2cfa865fc9c42dccc8dc34229da3be
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-23 11:21:32 +0530

    Jayla add button click redirect to opportunity detail page

--- src/components/dashboard/AuthRedirect.js
+++ src/components/dashboard/AuthRedirect.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route, useLocation } from 'react-router-dom';
 import { saveAuthTokenLocalStorage } from '../../helpers/LocalStorageHelper';
 
 function AuthRedirect({ ...options }) {
   const { search } = useLocation();
+  const [oppId, setOppId] = useState('');
   useEffect(() => {
     const authToken = new URLSearchParams(search).get('token');
+    const opportunityId = new URLSearchParams(search).get('opportunityId');
+    setOppId(opportunityId);
     saveAuthTokenLocalStorage({ authToken });
   }, []);
   return (
     <Route {...options}>
       {' '}
-      <Redirect to="/home" />
+      {oppId ? <Redirect to={`/followUps/opportunityDetails/${oppId}`} /> : <Redirect to="/home" />}
     </Route>
   );
 }
