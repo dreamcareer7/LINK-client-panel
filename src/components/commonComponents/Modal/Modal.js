24e1cdd97807005a636b296d92741189dfe380e2
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-01 13:33:19 +0530

    Modal added for delete

---
+++ src/components/commonComponents/Modal/Modal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Modal.scss';
+import {useOnClickOutside} from "../../../helpers/UseClickOutsideHook";
+
+
+// eslint-disable-next-line react/prop-types
+const Modal = ({ title, description, deleteData = () => {}, onClosePopup = () => {} }) => {
+  const popupRef = React.useRef();
+  useOnClickOutside(popupRef, onClosePopup);
+  const onDeleteClick = () => {
+    deleteData();
+  };
+  const onCloseButtonPress = () => {
+    onClosePopup();
+  };
+
+  return (
+    <div className="modal-main-container">
+      <div id="pop-up" ref={popupRef} className="modal-container">
+        <div className="modal-main-title">{title}</div>
+        <div className="modal-description">{description}</div>
+        <div className="buttons-row">
+          <button type="button" className="button white-button" onClick={onCloseButtonPress}>
+            Close
+          </button>
+          <button type="button" className="button danger-button" onClick={onDeleteClick}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
