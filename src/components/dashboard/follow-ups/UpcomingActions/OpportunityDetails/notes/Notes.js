4f8e6a9a95c13cdc499e146176eb76110bfbd41e
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-01 12:55:25 +0530

    Notes date format change to DD-MM-YYYY

--- src/components/dashboard/follow-ups/UpcomingActions/OpportunityDetails/notes/Notes.js
+++ src/components/dashboard/follow-ups/UpcomingActions/OpportunityDetails/notes/Notes.js
@@ -80,7 +80,7 @@ function Notes() {
                 />
               </div>
               <div className="note-time-stamp">
-                {moment(noteData.creationTime).format('MM-DD-YYYY | hh:mm A')}
+                {moment(noteData.creationTime).format('DD-MM-YYYY | hh:mm A')}
               </div>
             </div>
           ))
