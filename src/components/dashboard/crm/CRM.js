f380911d212d9e5bf8c85f7fea1f087d2b9445f8
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-03 10:24:04 +0530

    Crm filter date error message changed

--- src/components/dashboard/crm/CRM.js
+++ src/components/dashboard/crm/CRM.js
@@ -107,7 +107,7 @@ function Crm() {
     if (moment(startDate).isAfter(endDate)) {
       errorNotification('Please enter a valid date range');
     } else if (moment(endDate).isBefore(startDate)) {
-      errorNotification('Please enter to date after from date');
+      errorNotification('Please enter a valid date range');
     } else {
       const data = {
         stage: stage || undefined,
