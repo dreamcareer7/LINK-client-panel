bcca47c97cdcdf895c434252daccdb0477150f40
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-06 13:08:23 +0530

    loader component added

---
+++ src/components/commonComponents/Loader/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="dot dot-1" />
+      <div className="dot dot-2" />
+      <div className="dot dot-3" />
+      <div className="dot dot-4" />
+    </div>
+  );
+};
+
+export default Loader;
