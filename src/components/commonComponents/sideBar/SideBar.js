000c404a17205e5fb2c6d046fc6196fa0ec14d83
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-15 10:03:47 +0530

    Onclick follow up refresh page and reset filter

--- src/components/commonComponents/sideBar/SideBar.js
+++ src/components/commonComponents/sideBar/SideBar.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './sidebar.scss';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import linkfluencer from '../../../assets/images/linkfluencer.png';
 import home from '../../../assets/home.svg';
 /* import group from '../../../assets/group.png'; */
 import crm from '../../../assets/crm.svg';
 import reporting from '../../../assets/reporting.svg';
 import calendar from '../../../assets/calendar.svg';
+import { resetFilterData } from '../../../redux/actions/filterAction/FilterAction';
+import { getUpcomingActions } from '../../../redux/actions/followUpAction/FollowUpAction';
 
 function SideBar() {
+  const dispatch = useDispatch();
+  const followupData = useSelector(state => state.followUps);
+  const dealSizes = useMemo(
+    () =>
+      followupData && followupData.dealSize && followupData.dealSize[0]
+        ? followupData.dealSize[0]
+        : null,
+    [followupData]
+  );
+  const clearFilter = () => {
+    const dealData = {
+      endDealValue: dealSizes?.maxDealValue || 999999999,
+      startDealValue: dealSizes?.minDealValue || 1,
+    };
+    dispatch(resetFilterData(dealData));
+    const data = {
+      stages: [],
+      likelyHoods: [],
+    };
+    dispatch(getUpcomingActions(1, 9, data));
+  };
+
   return (
     <div>
       <div className="">
@@ -22,12 +47,16 @@ function SideBar() {
             <img alt="home" src={home} /> <span>Home</span>
           </NavLink>
 
-          <NavLink className="menu-item menu-link" to="/followups" replace>
+          <NavLink className="menu-item menu-link" to="/followups" replace onClick={clearFilter}>
             <img alt="followups" src={calendar} /> <span>Follow Ups</span>
           </NavLink>
 
           <NavLink className="menu-item menu-link" to="/crm" replace>
             <img alt="crm" src={crm} /> <span>CRM</span>
+          </NavLink>
+
+          <NavLink className="menu-item menu-link" to="/strategy" replace>
+            <img alt="crm" src={crm} /> <span>Strategy</span>
           </NavLink>
 
           <NavLink className="menu-item menu-link" to="/reporting" replace>
