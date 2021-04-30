3cb18dff29d307b30b0ec5a306f292c887d8c7e6
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-30 17:45:32 +0530

    app.js updated - with invalid url case to redirect to dashboard

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Layout from './components/commonComponents/layout/Layout';
 import SignupWithLinkedIn from './components/authentication/login-page/SignupWithLinkedIn';
 import FollowUps from './components/dashboard/follow-ups/FollowUps';
 import Crm from './components/dashboard/crm/CRM';
-import Reporting from './components/dashboard/reporting/Reporting';
 import AuthRedirect from './components/dashboard/AuthRedirect';
 import OpportunityDetails from './components/dashboard/follow-ups/UpcomingActions/OpportunityDetails/OpportunityDetails';
 import Account from './components/commonComponents/upperHeader/Account/Account';
@@ -30,10 +29,9 @@ const PrivateRoute = ({ component, ...options }) => {
     localStorage.getItem('userToken') !== null && localStorage.getItem('userToken').length !== 0;
 
   const finalComponent = isLoggedIn ? component : SignupWithLinkedIn;
-  if (options.path === '/' && isLoggedIn) {
+  if ((options.path === '/' || !options.path) && isLoggedIn) {
     return (
       <Route {...options}>
-        {' '}
         <Redirect to="/dashboard" />
       </Route>
     );
@@ -84,10 +82,10 @@ function App() {
             <PrivateRoute exact path="/" />
             <Layout>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute component={Dashboard} />
               <PrivateRoute exact path="/followups" component={FollowUps} />
               <PrivateRoute exact path="/crm" component={Crm} />
               <PrivateRoute exact path="/strategy" component={Strategy} />
-              <PrivateRoute exact path="/reporting" component={Reporting} />
               <PrivateRoute
                 exact
                 path="/followups/opportunityDetails/:id"
