395791d8e512b809f96a06090a4a4e06961fcdc6
dreamcareer7 <dreamcareer070@gmail.com>
2021-05-04 17:34:04 +0530

    Redux service added for fetch history

--- src/services/follow-up-service/historyService/HistoryService.js
+++ src/services/follow-up-service/historyService/HistoryService.js
@@ -2,7 +2,7 @@ import ApiService from '../../api-service/ApiService';
 import { HISTORY_URL } from '../../../constants/UrlConstant';
 
 const HistoryService = {
-  fetchConversation: (id, data) =>
-    ApiService.putData(HISTORY_URL.FETCH_CONVERSATION_URL + id, data),
+  fetchConversation: (id, data, params) =>
+    ApiService.putData(`${HISTORY_URL.FETCH_CONVERSATION_URL}${id}`, data ,{params}),
 };
 export default HistoryService;
