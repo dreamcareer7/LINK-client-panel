9d3c19029226a4b6aabca1de80eed417dafeba88
dreamcareer7 <dreamcareer070@gmail.com>
2022-01-06 11:10:00 +0530

    latest

--- src/components/authentication/login-page/SignupWithLinkedIn.js
+++ src/components/authentication/login-page/SignupWithLinkedIn.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import linkFluencer from '../../../assets/images/linkfluencer.png';
-import signInLinkedIn from '../../../assets/images/Sign-in-Large---Default.png';
+// import signInLinkedIn from '../../../assets/images/Sign-in-Large---Default.png';
 import './login.scss';
-import { BASE_URL, LINKEDIN_CLIENT_ID } from '../../../constants/UrlConstant';
+// import { BASE_URL, LINKEDIN_CLIENT_ID } from '../../../constants/UrlConstant';
 import { errorNotification } from '../../../constants/Toast';
 import { useQueryParams } from '../../../helpers/GetQueryParamHook';
 
 function SignupWithLinkedIn() {
-  const onClickSignUp = () => {
-    window.location = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${BASE_URL}client-auth/sign-up&state=fooobar&scope=r_emailaddress,r_liteprofile`;
-  };
+  // const onClickSignUp = () => {
+  //   window.location = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${BASE_URL}client-auth/sign-up&state=fooobar&scope=r_emailaddress,r_liteprofile`;
+  // };
   const { message } = useQueryParams();
   const history = useHistory();
   useEffect(() => {
@@ -26,9 +26,14 @@ function SignupWithLinkedIn() {
     <div className="login-content-container">
       <img alt="linkfluencer" src={linkFluencer} className="logo" />
 
-      <button type="button" className="sign-up-button" onClick={onClickSignUp}>
+      {/* <button type="button" className="sign-up-button" onClick={onClickSignUp}>
         <img src={signInLinkedIn} />
-      </button>
+      </button> */}
+      <div className='text-center'><p>You are currently logged out of Jayla,<br />
+to access your account and sync<br />
+with LinkedIn, click on the Chrome<br />
+Extension icon to the top right<br />
+and click sign in.</p></div>
     </div>
   );
 }
